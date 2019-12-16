ec83a81ebae9de4a5484a40a8f3e55a227e9e01b
Antkeo1 <sleav143@gmail.com>
2019-12-16 11:40:11 -0500

    heroku is connected

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.get('/', (req, res) => {
     res.send('homepage')
 })
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT)
+app.listen(process.env.PORT || 5000)
