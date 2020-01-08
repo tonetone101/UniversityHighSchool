23460180bcaf07d456b027946929ff3096894b42
Antkeo1 <sleav143@gmail.com>
2020-01-08 06:52:58 -0500

    translated page for khmer and spanish Partners

--- khmer/routes/partners.js
+++ khmer/routes/partners.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const {getkhmerPartners, createkhmerPartners, khmerPartnersById, singlekhmerPartners, photo, updatekhmerPartners, deletekhmerPartners} = require('../controllers/khmerPartners')
+const {getPartners, createPartners, partnersById, singlePartners, photo, updatePartners, deletePartners} = require('../controllers/khmerPartners')
 // const {createkhmerPartnersValidator} = require('../validator')
 // const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../../general/controllers/user');
 
 router = express.Router()
 
-router.get('/khmerPartners', getkhmerPartners)
-router.get('/khmerPartners/:khmerPartnersId', singlekhmerPartners)
-router.post('/khmerPartners/new/:userId',  createkhmerPartners)
-router.put('/khmerPartners/edit/:khmerPartnersId',  updatekhmerPartners)
-router.delete('/khmerPartners/delete/:khmerPartnersId',  deletekhmerPartners);
+router.get('/khmerPartners', getPartners)
+router.get('/khmerPartners/:khmerPartnersId', singlePartners)
+router.post('/khmerPartners/new/:userId',  createPartners)
+router.put('/khmerPartners/edit/:khmerPartnersId',  updatePartners)
+router.delete('/khmerPartners/delete/:khmerPartnersId',  deletePartners);
 
 router.get('/khmerPartners/photo/:khmerPartnersId', photo);
 
 router.param('userId', userById);
-router.param('khmerPartnersId', khmerPartnersById);
+router.param('khmerPartnersId', partnersById);
 
 module.exports = router
