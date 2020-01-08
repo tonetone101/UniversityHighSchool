aa3765cbb377dbf82bc5d46f4d92196582cb6f9f
Antkeo1 <sleav143@gmail.com>
2020-01-08 14:03:47 -0500

    restlying homepage

--- client/src/english/carousel/Carousel.js
+++ client/src/english/carousel/Carousel.js
@@ -286,7 +286,7 @@ class Carol extends Component {
                         }
                     </div>
                                         </div>
-                        <div className='row container'>
+                        <div className='row container' style={{marginLeft: '10px'}}>
                            
                                         <img 
                                             style={{ height: "300px", width: "200px" }}
