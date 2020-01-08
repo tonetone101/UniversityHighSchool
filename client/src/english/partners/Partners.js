fbac7cb6f7e5cbc2a4347088a8adb230cc3d93a4
Antkeo1 <sleav143@gmail.com>
2020-01-07 21:23:16 -0500

    added menu to partners page

--- client/src/english/partners/Partners.js
+++ client/src/english/partners/Partners.js
@@ -13,8 +13,16 @@ class Partners extends Component {
             page: 1,
             error: '',
             searching: false,
+            spanishPage: false,
+            englishPage: false,
+            khmerPage: false
         };
     }
+
+    renderUser = () => {
+        this.setState({user: isAuthenticated().user })
+    }
+
 
     loadPartners = page => {
         list(page).then(data => {
@@ -31,6 +39,120 @@ class Partners extends Component {
 
     componentDidMount() {
         this.loadPartners(this.state.partners)
+        this.renderUser()
+    }
+
+    componentWillReceiveProps() {
+        this.renderUser()
+    }
+
+    translateSpanish = () => {
+        this.setState({spanishPage: true, englishPage: false, khmerPage: false})
+    }
+
+    translateEnglish = () => {
+        this.setState({englishPage: true, spanishPage: false, khmerPage: false})
+    }
+
+    translateKhmer = () => {
+        this.setState({khmerPage: true, spanishPage: false, englishPage: false,})
+    }
+
+    renderTopHeader = () => {
+        return (
+            <div>
+                <Navbar id='topHeader' collapseOnSelect expand="lg" variant="dark" >
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto " >
+                    <DropdownButton id="dropdown-basic-button" title="translator"  >
+                                <Dropdown.Item ><a onClick={this.translateSpanish}>Spanish</a>
+                                </Dropdown.Item>
+                                <Dropdown.Item ><a onClick={this.translateKhmer}>Cambodian</a>
+                                </Dropdown.Item>
+                                <Dropdown.Item><a>Hmong</a></Dropdown.Item>
+
+                                <Dropdown.Item><a onClick={this.translateEnglish}>English</a></Dropdown.Item>
+
+                                <Dropdown.Item><a>Portuguese</a></Dropdown.Item>
+                            
+                            </DropdownButton>
+                        
+                        {
+                            !this.state.user && (
+                               <nav className='row'>
+                                <Nav.Link >
+                                    <Link className='ml-3' to='/signin' style={{color: 'black'}}>
+                                        Sign In 
+                                    </Link>
+                                </Nav.Link>
+                                <Nav.Link>
+                                    <Link style={{color: 'black'}} to='/signup' >
+                                        Sign Up
+                                    </Link>
+                                </Nav.Link>
+                               </nav>
+                            )
+                        }
+                        
+                        {
+                            this.state.user && (
+                                <Nav.Link>
+                                    <a style={{color: 'black'}}  onClick={() => signout(() => {
+                                        this.props.history.push('/')
+                                    })}>
+                                        Sign Out
+                                    </a>
+                                </Nav.Link>
+                            )
+                        }
+                      
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            </div>
+        )
+    }
+
+    renderMenu = () => {
+        return (
+            <div>
+                 <Navbar id='menu' collapseOnSelect expand="lg" variant="dark"  >
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    
+                    <Nav className="mr-auto " className="col d-flex justify-content-around align-items-baseline">
+                         <div id='link'>                        
+                            <Nav.Link href="#features"><Link style={{color: 'white'}} to='/'>Home</Link></Nav.Link>
+                        </div>
+
+                       <div id='link'>                
+                           <Nav.Link href="#features"><Link style={{color: 'white'}} to='/faculty'>Faculty</Link></Nav.Link>
+                        </div>
+                        <Nav.Link href="#features"><Link style={{color: 'white'}} to='/student'>Students</Link></Nav.Link>
+                        
+                        
+                        <div id='link'>                        
+                            <Nav.Link href="#features"><Link style={{color: 'white'}} to='/admission'>Admission</Link></Nav.Link>
+                        </div>
+
+                        <div id='link'>                        
+                            <Nav.Link href="#features"><Link style={{color: 'white'}} to='/partners'>Our Partners</Link></Nav.Link>
+                        </div>
+
+                        <div id='link'>                        
+                            <Nav.Link href="#features"><Link style={{color: 'white'}} to='/images'>Gallery</Link></Nav.Link>
+                        </div>
+
+                        <div id='link'>                        
+                            <Nav.Link href="#features"><Link style={{color: 'white'}} to='/events'>Upcoming Events</Link></Nav.Link>
+                        </div>
+                    
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            </div>
+        )
     }
 
     renderPartners = partners => {
@@ -70,31 +192,43 @@ class Partners extends Component {
     };
 
     render() {
-        const { user, partners, error } = this.state;
+        const { user, partners, error, spanishPage, khmerPage, englishPage} = this.state;
+        if(spanishPage) {
+            return <Redirect to={`/spanish/partners`} />
+         } else if (englishPage) {
+             return <Redirect to={'/partners'} />
+         } else if (khmerPage) {
+            return <Redirect to={'/khmer/partners'} />
+        } 
+
 
 
         return (
-            <div className="container">
-                <div className='row mt-4 mb-3'>
-                    <h2 className="col-md-6" style={{borderBottom: 'solid black 1px'}}>
-                        Our Partners
-                        {!partners.length ? "Loading..." : ""}
-                    </h2>
+            <div>
+                {this.renderTopHeader()}
+                {this.renderMenu()}
+                <div className="container">
+                    <div className='row mt-4 mb-3'>
+                        <h2 className="col-md-6" style={{borderBottom: 'solid black 1px'}}>
+                            Our Partners
+                            {!partners.length ? "Loading..." : ""}
+                        </h2>
 
-                    <hr/>
+                        <hr/>
+                    </div>
+                    {
+                        isAuthenticated() && isAuthenticated().user.role === 'admin' && (
+                            <div>
+                                <Link className='mb-5' to='/new/partners'>Add new partner</Link>
+                            </div>
+                        )
+                    }
+                
+                    <div>               
+                        {this.renderPartners(partners)}
+                    </div>   
+                
                 </div>
-                {
-                    isAuthenticated() && isAuthenticated().user.role === 'admin' && (
-                        <div>
-                            <Link className='mb-5' to='/new/partners'>Add new partner</Link>
-                        </div>
-                    )
-                }
-               
-                <div>               
-                    {this.renderPartners(partners)}
-                 </div>   
-               
             </div>
         );
     }
