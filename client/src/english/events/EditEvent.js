2cde108e987268cd2de8a11bdd6a5a68f5ddd2a4
Antkeo1 <sleav143@gmail.com>
2020-01-13 13:24:46 -0500

    building school board members

--- client/src/english/events/EditEvent.js
+++ client/src/english/events/EditEvent.js
@@ -189,7 +189,7 @@ class EditEvent extends Component {
                         {/* <img style={{height: '200px', width: 'auto'}} className='img-thumbnail' src={`${process.env.REACT_APP_API_URL}/event/photo/${id}`} onError={i => (i.target.src = ``)} alt='' /> */}
 
 
-                        {this.editEventForm(url, title, date, time, body)}
+                        {this.editEventForm(url, title, time, date, body)}
             </div>
         )
     }
