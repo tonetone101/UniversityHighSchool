013af57a92bf845223f2398b277f0c57e6d2f606
Antkeo1 <sleav143@gmail.com>
2020-01-25 14:48:53 -0500

    added props to english admissions

--- client/src/english/student/Admission.js
+++ client/src/english/student/Admission.js
@@ -211,11 +211,7 @@ class Admission extends Component {
         return (
             <div>
                 <h3>{admission.title}</h3>
-                <div>
-                    {this.state.loading &&
-                        <AdmissionNews admissionId={admission._id} comments={comments.reverse()} updateComments={this.updateComments} />
-                    }
-                </div>
+                
             </div>
         )
     }
@@ -305,6 +301,11 @@ class Admission extends Component {
 
                         <div className='col-md-6 text-center'>
                             {this.renderRequirements(admission)}
+                            <div>
+                                {this.state.loading &&
+                                    <AdmissionNews admissionId={admission._id} comments={comments.reverse()} updateComments={this.updateComments} />
+                                }
+                            </div>
                         </div>
 
                                      
