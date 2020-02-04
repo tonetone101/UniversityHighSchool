7c1fe9e53609238acaea8b71ec8d91e86d772c32
Antkeo1 <sleav143@gmail.com>
2020-02-04 10:57:24 -0500

    setting up different user roles for sign up

--- client/src/english/carousel/Carousel.js
+++ client/src/english/carousel/Carousel.js
@@ -14,6 +14,7 @@ class Carol extends Component {
 
     renderUser = () => {
         this.setState({user: isAuthenticated().user })
+        console.log(isAuthenticated().user)
     }
 
     componentDidMount() {
