d55b3e89fc1196f51c58a3dd9e1fc52219ff0bb2
Antkeo1 <sleav143@gmail.com>
2020-02-04 10:50:35 -0500

    setting up different user roles for sign up

--- client/src/english/carousel/Carousel.js
+++ client/src/english/carousel/Carousel.js
@@ -208,7 +208,7 @@ class Carol extends Component {
                             }
 
                             {
-                                isAuthenticated() && isAuthenticated().user.code === '2609' && (
+                                isAuthenticated() && isAuthenticated().user.code == '2609' && (
                                     <Link to={`/edit/carousel/${carousel._id}`} className='text-center btn btn-primary mt-4 mb-4'>Update for principle</Link>
                                 )
                             }
