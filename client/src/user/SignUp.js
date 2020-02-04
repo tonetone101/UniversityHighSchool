29b941cb2b5bbda5d6b4640a07bbc33269db2ca0
Antkeo1 <sleav143@gmail.com>
2020-02-04 12:21:45 -0500

    setting up different user roles for sign up

--- client/src/user/SignUp.js
+++ client/src/user/SignUp.js
@@ -77,14 +77,16 @@ class Signup extends Component {
             {code === '2609' ? (
                 <div className='form-group'>
                     <label className='text-muted'>Role</label>
-                    <input onChange={this.handleChange('role')} type='text' className='form-control' value='principle' />
+                    <input type='text' className='form-control' value={() => this.setState({
+                        role: 'principle'
+                    })} />
                 </div>
             ) : (null)}
 
             {code === '1017' ? (
                 <div className='form-group'>
                     <label className='text-muted'>Role</label>
-                    <input onChange={this.handleChange('role')} type='text' className='form-control' value='director of operations' />
+                    <input type='text' className='form-control' value='director of operations' />
                 </div>
             ) : (null)}
             
