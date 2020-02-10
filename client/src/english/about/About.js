ceac0aee265c4b47d4cd46bfa84a6c8a66ac1a1b
Antkeo1 <sleav143@gmail.com>
2020-02-10 10:54:57 -0500

    styling about us section

--- client/src/english/about/About.js
+++ client/src/english/about/About.js
@@ -23,7 +23,7 @@ class About extends Component {
     }
 
     isActive = (history, path) => {
-      if (this.props.history.location.pathname === path) {
+      if (this.props.history.location.pathname === path) return {
         color: '#ff9900'
       } else {
         color: '#ffffff'
