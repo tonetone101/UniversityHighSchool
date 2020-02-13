07f127d0371c2eb311f631ba2eeeba97f79720e7
Antkeo1 <sleav143@gmail.com>
2020-02-13 12:43:44 -0500

    styling about us section

--- client/src/english/about/About.js
+++ client/src/english/about/About.js
@@ -56,7 +56,7 @@ class About extends Component {
 
         return (
             <div className='container mt-5' style={{textIndent: '50px'}}>
-                <Card border='dark' style={{borderRadius: '90%'}}>
+                <Card border='dark' style={{borderRadius: '45%'}}>
                     <Card.Body>
                     <Card.Header className="font-italic mark">
                       About Us
