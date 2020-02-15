6dcecbf7bcf207e28e44b4cc4b6ae8cdd5275ab4
Antkeo1 <sleav143@gmail.com>
2020-02-15 11:53:07 -0500

    styling about us section

--- client/src/english/schoolBoard/Main.js
+++ client/src/english/schoolBoard/Main.js
@@ -91,9 +91,9 @@ class Main extends Component {
                          <h3>Upcoming Meetings:</h3> 
                         {schoolBoardMeeting.reverse().map((schoolBoardMeeting,  i) => (
                         
-                            <div id='boardList' key={i} >
-                                <ListGroup id='boardList' variant="flush">
-                                    <ListGroup.Item id='boardList'> 
+                            <div style={{backgroundColor: '#b7b7b7'}} key={i} >
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item> 
                                         <Link onClick={() => { 
                                             window.open(`${schoolBoardMeeting.url}`) 
                                             }}  
