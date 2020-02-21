00c26e448dcd199da9097c64dc029d2b16ab767c
Antkeo1 <sleav143@gmail.com>
2020-02-20 20:37:20 -0500

    styling sidebar

--- client/src/english/sideBar/SideBar.js
+++ client/src/english/sideBar/SideBar.js
@@ -31,19 +31,19 @@ class SideBar extends Component {
 
         return (
                 <div className='col-md-4 column text-center mt-5'>
-                    <div className='mb-5'>
+                    <div className='mb-2'>
                         <Link style={isActive(history, '/partners')} to='/partners'>
                                 Our Partners
                         </Link>
                     </div>
 
-                    <div className='mb-5'>
+                    <div className='mb-2'>
                         <Link style={isActive(history, '/about')} className='mt-4' to='/about'>
                                 About us
                         </Link>
                     </div>
 
-                    <div className='mb-5'>
+                    <div className='mb-2'>
                         <Link style={isActive(history, '/hr')} className='mt-4' to='/hr'>
                                 Human Resource
                         </Link>
