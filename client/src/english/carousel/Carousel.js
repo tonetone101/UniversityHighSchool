f6d326260f74ea0a88d89d616dbd95e651fed59f
Antkeo1 <sleav143@gmail.com>
2020-03-09 13:42:28 -0400

    fixing navbar

--- client/src/english/carousel/Carousel.js
+++ client/src/english/carousel/Carousel.js
@@ -6,6 +6,7 @@ import { isAuthenticated} from '../../auth'
 import {Animated} from 'react-animated-css'
 import { Card, Button } from 'react-bootstrap';
 import Header from '../header/Header'
+import SideNav from '../header/SideNav'
 
 class Carol extends Component {
     state = {
@@ -251,7 +252,9 @@ class Carol extends Component {
                                 </div>
                                 ) : (
                                     <div>
+                                        <SideBar />
                                         <iframe width="100%" height="630" src="https://www.youtube.com/embed/Y3K1rtw5omA?autoplay=1&rel=1&controls=0&loop=1&playlist=Y3K1rtw5omA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        
                                     </div>
                                     
                                 )
