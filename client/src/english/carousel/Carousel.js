d6c85f19691d9ca59e6cf21be77489413551970c
Antkeo1 <sleav143@gmail.com>
2020-03-14 07:45:05 -0400

    working on homepage

--- client/src/english/carousel/Carousel.js
+++ client/src/english/carousel/Carousel.js
@@ -305,29 +305,31 @@ class Carol extends Component {
                                         
                    
                 </div>
+                    
                         <div>
-                            <h4>
-                               OUR MISSION AND GOAL
-                            </h4>
-                        </div>
-                        <div className='row container' style={{marginLeft: '150px', marginTop: '150px'}}>
-                           
-                            <img 
-                                id='homeImage'
-                                style={{ height: "300px", width: "200px" }}
-                                className="mt-4 col-sm-6"
-                                src={require("../../images/uhsMission.png")}
-                                alt="Second slide" 
-                                
-                            />   
-                            <img 
-                                id='homeImage'
-                                style={{ height: "300px", width: "200px" }}
-                                className="mt-4 col-sm-6"
-                                src={require("../../images/uhsVision.png")}
-                                alt="Second slide" 
-                                
-                            />  
+                            <div className='text-center'>
+                                <h4>
+                                OUR MISSION AND GOAL
+                                </h4>
+                            </div>
+                            <div className='row container' style={{marginLeft: '150px', marginTop: '150px'}}>
+                                <img 
+                                    id='homeImage'
+                                    style={{ height: "300px", width: "200px" }}
+                                    className="mt-4 col-sm-6"
+                                    src={require("../../images/uhsMission.png")}
+                                    alt="Second slide" 
+                                    
+                                />   
+                                <img 
+                                    id='homeImage'
+                                    style={{ height: "300px", width: "200px" }}
+                                    className="mt-4 col-sm-6"
+                                    src={require("../../images/uhsVision.png")}
+                                    alt="Second slide" 
+                                    
+                                />  
+                            </div>
                            
                         </div>
                         
