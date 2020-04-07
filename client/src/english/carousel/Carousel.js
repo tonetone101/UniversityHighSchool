1bfa3328853b0aad8331027a83feb4a76306731f
Antkeo1 <sleav143@gmail.com>
2020-04-07 12:52:39 -0400

    adding a new uhs video

--- client/src/english/carousel/Carousel.js
+++ client/src/english/carousel/Carousel.js
@@ -282,7 +282,7 @@ class Carol extends Component {
                                 </div>
                                 ) : (
                                     <div id='video' >
-                                        <iframe width="100%" height="630" src="https://www.youtube.com/embed/Y3K1rtw5omA?autoplay=1&rel=1&controls=0&loop=1&playlist=Y3K1rtw5omA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                        <iframe width="100%" height="630" src={'https://drive.google.com/file/d/1DmGcmLu_RUtge8SajCu1GSKbXxTCtsoN/view?ts=5e8caebe'}></iframe>
                                         
                                     </div>
                                     
